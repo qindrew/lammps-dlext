8401d8a02f5ea36b0492b2eb04741c7e9927fac1
Pablo Zubieta <8410335+pabloferz@users.noreply.github.com>
2023-03-21 20:10:33 -0500

    Some cleanup

--- dlext/include/Sampler.h
+++ dlext/include/Sampler.h
@@ -23,12 +23,13 @@ namespace cxx11 = cxx11utils;
 struct access_location {
     //! The enum
     enum Enum {
-        host,   //!< Ask to acquire the data on the host
+        host,  //!< Ask to acquire the data on the host
 #ifdef KOKKOS_ENABLE_CUDA
         device  //!< Ask to acquire the data on the device
 #endif
     };
 };
+
 using AccessLocation = access_location::Enum;
 const auto kOnHost = access_location::host;
 #ifdef KOKKOS_ENABLE_CUDA
@@ -36,14 +37,15 @@ const auto kOnDevice = access_location::device;
 #endif
 
 //! Specify how the data is to be accessed
-struct access_mode  {
+struct access_mode {
     //! The enum
     enum Enum {
-        read,       //!< Data will be accessed read only
+        read,  //!< Data will be accessed read only
         readwrite,  //!< Data will be accessed for read and write
-        overwrite   //!< The data is to be completely overwritten during this acquire
+        overwrite  //!< The data is to be completely overwritten during this acquire
     };
 };
+
 using AccessMode = access_mode::Enum;
 const auto kRead = access_mode::read;
 const auto kReadWrite = access_mode::readwrite;
@@ -58,29 +60,24 @@ using namespace FixConst;
   to advance atom positions based on the instantaneous values of the CVs
   NOTE: A closely related example is the existing FixExternal in LAMMPS
     (docs.lammps.org/fix_external.html)
-  
+
 */
 
-template <typename ExternalUpdater, template <typename> class Wrapper, class DeviceType>
+template <typename ExternalUpdater, class DeviceType>
 class DEFAULT_VISIBILITY Sampler : public FixExternal {
 public:
     //! Constructor
-    Sampler(LAMMPS* lmp, int narg, char** arg,
-            ExternalUpdater update_callback,
-            AccessLocation location,
-            AccessMode mode
-    ) : FixExternal(lmp, narg, arg),
-    _update_callback { update_callback },
-    _location { location },
-    _mode { mode }
-    { 
+    Sampler(LAMMPS* lmp, int narg, char** arg, ExternalUpdater update_callback, AccessLocation location, AccessMode mode)
+        : FixExternal(lmp, narg, arg)
+        , _update_callback { update_callback }
+        , _location { location }
+        , _mode { mode }
+    {
         kokkosable = 1;
-        atomKK = (AtomKokkos *) atom;
-
+        atomKK = dynamic_cast<AtomKokkos*>(atom);
         execution_space = ExecutionSpaceFromDevice<DeviceType>::space;
-        
-        datamask_read   = X_MASK | V_MASK | F_MASK | TYPE_MASK | IMAGE_MASK | OMEGA_MASK | MASK_MASK | TORQUE_MASK | ANGMOM_MASK;
-        datamask_modify = X_MASK | V_MASK | F_MASK | OMEGA_MASK | TORQUE_MASK | ANGMOM_MASK;
+        datamask_read = X_MASK | V_MASK | F_MASK | TYPE_MASK | IMAGE_MASK | OMEGA_MASK | MASK_MASK | TORQUE_MASK | ANGMOM_MASK;
+        datamask_modify = datamask_read;  // to enable restores from PySAGES
     }
 
     int setmask() override
@@ -104,7 +101,7 @@ public:
     template <typename Callback>
     void forward_data(Callback callback, AccessLocation location, AccessMode mode, TimeStep n)
     {
-        atomKK->sync(execution_space,datamask_read);
+        atomKK->sync(execution_space, datamask_read);
 
         x = atomKK->k_x.template view<DeviceType>();
         v = atomKK->k_v.template view<DeviceType>();
@@ -114,7 +111,7 @@ public:
         tag = atomKK->k_tag.template view<DeviceType>();
 
         if (atomKK->omega_flag)
-            omega  = atomKK->k_omega.template view<DeviceType>();
+            omega = atomKK->k_omega.template view<DeviceType>();
 
         if (atomKK->angmom_flag)
             angmom = atomKK->k_angmom.template view<DeviceType>();
@@ -135,8 +132,7 @@ public:
 
         // callback will be responsible for advancing the simulation for n steps
 
-    //    callback(pos_capsule, vel_capsule, rtags_capsule, img_capsule, force_capsule, n);
-    
+        //    callback(pos_capsule, vel_capsule, rtags_capsule, img_capsule, force_capsule, n);
     }
 
     auto get_positions()
@@ -153,8 +149,8 @@ public:
 
     auto get_net_forces()
     {
-       int nlocal = atom->nlocal;
-       return wrap(f.data(), _location, _mode, nlocal, 3);
+        int nlocal = atom->nlocal;
+        return wrap(f.data(), _location, _mode, nlocal, 3);
     }
 
     auto get_type()
@@ -172,8 +168,7 @@ public:
     DLDevice dldevice(bool gpu_flag);
 
     template <typename T>
-    DLManagedTensorPtr wrap(const T* data, const AccessLocation location, const AccessMode mode,
-                        const int num_particles, int64_t size2 = 1, uint64_t offset = 0, uint64_t stride1_offset = 0);
+    DLManagedTensorPtr wrap(const T* data, const AccessLocation location, const AccessMode mode, const int num_particles, int64_t size2 = 1, uint64_t offset = 0, uint64_t stride1_offset = 0);
 
 private:
     ExternalUpdater _update_callback;
@@ -199,7 +194,7 @@ private:
 struct Positions final {
     static DLManagedTensorPtr from(LAMMPS* lmp, AccessLocation location, AccessMode mode)
     {
-       
+
        atomKK->sync(execution_space,datamask_read);
         return wrap(x, location, mode, 3);
     }
@@ -232,8 +227,8 @@ struct NetForces final {
 };
 */
 
-} // namespace dlext
+}  // namespace dlext
 
-} // namespace LAMMPS_NS
+}  // namespace LAMMPS_NS
 
 #endif  // DLEXT_SAMPLER_H_
