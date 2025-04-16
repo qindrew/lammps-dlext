466de3c9a1bdf130a4591a47c230b1d50c5a2fb7
qindrew <168153413+qindrew@users.noreply.github.com>
2025-04-16 19:27:47 -0400

    Update FindLAMMPSTools.cmake

--- cmake/Modules/FindLAMMPSTools.cmake
+++ cmake/Modules/FindLAMMPSTools.cmake
@@ -312,6 +312,7 @@ target_include_directories(LAMMPS_src INTERFACE "${lammps_SOURCE_DIR}/src")
 #if(Kokkos_FOUND)
 #    message(STATUS "Kokkos support has been enabled (version ${Kokkos_VERSION})")
 target_include_directories(LAMMPS_src INTERFACE "${lammps_SOURCE_DIR}/src/KOKKOS")
+target_include_directories(LAMMPS_src INTERFACE "${lammps_SOURCE_DIR}/lib/kokkos")
 #else()
 #    message(STATUS
 #        "Kokkos support is not enabled. If you built LAMMPS with Kokkos,"
